6c8173887856085295b206fa20562092ec1ab5e2
Alexis Parave <aparavev@gmail.com>
2022-09-10 18:07:54 -0500

    FEAT: add games_count column to companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_10_000450) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_10_230544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_10_000450) do
     t.integer "critics_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "games_count", default: 0
     t.index ["name"], name: "index_companies_on_name", unique: true
   end
 
