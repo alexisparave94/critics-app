941c49c59daa714aabf4dc010ebe7bffe8737dd1
Alexis Parave <aparavev@gmail.com>
2022-09-15 15:32:30 -0500

    FEAT: add validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,15 @@
 class Game < ApplicationRecord
+  # Associations
   belongs_to :parent, class_name: "Game",
                       optional: true
   has_many :games, foreign_key: "parent_id",
                    dependent: :nullify,
                    inverse_of: "parent"
+
+  # Validations
+
+  validates :name, presence: true
+  validates :summary, length: { in: 10..100 }, allow_nil: true
+  validates :release_date, comparison: { less_than: Time.zone.now }, allow_nil: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 end
