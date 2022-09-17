f268823b0589cf10b144ff0bf971294f74089e26
Alexis Parave <aparavev@gmail.com>
2022-09-17 13:20:45 -0500

    FEAT: create join table game_genres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_181230) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_17_181711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_181230) do
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_games_on_name", unique: true
     t.index ["parent_id"], name: "index_games_on_parent_id"
+  end
+
+  create_table "games_genres", id: false, force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "genre_id", null: false
   end
 
   create_table "genres", force: :cascade do |t|
