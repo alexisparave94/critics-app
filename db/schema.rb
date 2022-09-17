a5322d6597ddc7d1411f8fe8943dce67fed393fc
Alexis Parave <aparavev@gmail.com>
2022-09-16 20:42:59 -0500

    FEAT: create involved company model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_15_194934) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_17_004906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,5 +40,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_194934) do
     t.index ["parent_id"], name: "index_games_on_parent_id"
   end
 
+  create_table "involved_companies", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.bigint "game_id", null: false
+    t.boolean "developer"
+    t.boolean "publisher"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id", "game_id"], name: "index_involved_companies_on_company_id_and_game_id", unique: true
+    t.index ["company_id"], name: "index_involved_companies_on_company_id"
+    t.index ["game_id"], name: "index_involved_companies_on_game_id"
+  end
+
   add_foreign_key "games", "games", column: "parent_id"
+  add_foreign_key "involved_companies", "companies"
+  add_foreign_key "involved_companies", "games"
 end
