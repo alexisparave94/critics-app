a5486ad749d88a13d5191565bef2a61716875793
Alexis Parave <aparavev@gmail.com>
2022-09-17 02:23:30 -0500

    FEAT: add functionality to add a developer or publisher involved company for a game

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "games#index"
-  resources :games
+  resources :games do
+    resources :involved_companies, only: %i[create]
+  end
   resources :companies
 end
