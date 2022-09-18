e2fc39ca0e9ae41e56b88a165093b1639afa4dac
Alexis Parave <aparavev@gmail.com>
2022-09-17 23:48:14 -0500

    FEAT: create platform model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_17_181711) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_18_044652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_181711) do
     t.index ["company_id", "game_id"], name: "index_involved_companies_on_company_id_and_game_id", unique: true
     t.index ["company_id"], name: "index_involved_companies_on_company_id"
     t.index ["game_id"], name: "index_involved_companies_on_game_id"
+  end
+
+  create_table "platforms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_platforms_on_name", unique: true
   end
 
   add_foreign_key "games", "games", column: "parent_id"
