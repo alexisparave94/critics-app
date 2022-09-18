02d91d30922254677b6f40b8154c081599d1a237
Alexis Parave <aparavev@gmail.com>
2022-09-17 23:50:31 -0500

    FEAT: create join table for games and platforms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_18_044652) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_18_044851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_18_044652) do
   create_table "games_genres", id: false, force: :cascade do |t|
     t.bigint "game_id", null: false
     t.bigint "genre_id", null: false
+  end
+
+  create_table "games_platforms", id: false, force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "platform_id", null: false
   end
 
   create_table "genres", force: :cascade do |t|
